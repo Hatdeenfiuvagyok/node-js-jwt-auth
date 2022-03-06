3482ded68642423ddc8f0c2f25c4dcf1c6f444b6
Hatdeenfiuvagyok <ug.daviid@gmail.com>
2022-03-06 13:52:12 +0100

    new

--- app/routes/sajat.backend.js
+++ app/routes/sajat.backend.js
@@ -13,6 +13,21 @@ module.exports = function(app) {
   //Saját backend végpontok
 
   
+const fileupload = require("express-fileupload");
+
+app.use(fileupload());
+  app.post("/upload", (req, res) => {
+    const newpath = "./kepek/";
+    const file = req.files.file;
+    const filename = file.name;
+  
+    file.mv(`${newpath}${filename}`, (err) => {
+      if (err) {
+        return res.status(500).send({ message: "File upload failed", code: 200 });
+      }
+        return res.status(200).send({ message: "File Uploaded", code: 200 });
+    });
+  });
 
 
 
@@ -243,6 +258,70 @@ app.get('/Alkar', (req, res) => {
 
 
 
+app.get('/izomcsoportok', (req, res) => {
+
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from izomcsoportok', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+    res.send(rows)
+  })
+  
+  connection.end()
+
+})
+
+
+app.get('/taplalekkiegeszitok', (req, res) => {
+
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT kiegeszitok_nev, kiegeszitok_leiras FROM kiegeszitok ORDER BY etrend_id ASC;', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log(rows)
+    res.send(rows)
+  })
+  
+  connection.end()
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.post('/adat_felvitel', (req, res) => {
   var mysql = require('mysql')
@@ -255,7 +334,7 @@ app.post('/adat_felvitel', (req, res) => {
   
   connection.connect()
 
-  connection.query("INSERT INTO izomcsoport_kepek VALUES (NULL, 'teszt1.png', '1', '"+req.body.bevitel3+"');", function (err, rows, fields) {
+  connection.query("INSERT INTO izomcsoport_kepek VALUES (NULL, '"+req.body.bevitel1+"', '"+req.body.bevitel2+"', '"+req.body.bevitel3+"');", function (err, rows, fields) {
     if (err) throw err
   
     console.log("Sikeres felvitel!")
@@ -267,37 +346,6 @@ app.post('/adat_felvitel', (req, res) => {
 
 })  
 
-
-
-
-
-  
-
-
-
-
-  app.post('/adat_torles', (req, res) => {
-    var mysql = require('mysql')
-    var connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'zarodolgozat'
-    })
-    
-    connection.connect()
-  
-    connection.query("DELETE FROM uzenetek WHERE uzenet_id=" +req.body.bevitel1, function (err, rows, fields) {
-      if (err) throw err
-    
-      console.log("Sikeres törlés!")
-  
-      res.send("Sikeres törlés!")
-    })
-    
-    connection.end()    
-  
-  })  
 
 
   
@@ -381,6 +429,283 @@ app.get('/tema', (req, res) => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.post('/adat_torles', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+
+  connection.query("DELETE FROM uzenetek WHERE uzenet_id=" +req.body.bevitel1, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log("Sikeres törlés!")
+
+    res.send("Sikeres törlés!")
+  })
+  
+  connection.end()    
+
+})  
+
+
+
+app.post('/adat_torles_mell', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+
+  connection.query("DELETE FROM izomcsoport_kepek WHERE kepek_id=" +req.body.bevitel1, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log("Sikeres törlés!")
+
+    res.send("Sikeres törlés!")
+  })
+  
+  connection.end()    
+
+})  
+
+
+
+app.post('/adat_torles_bicepsz', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+
+  connection.query("DELETE FROM izomcsoport_kepek WHERE kepek_id=" +req.body.bevitel1, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log("Sikeres törlés!")
+
+    res.send("Sikeres törlés!")
+  })
+  
+  connection.end()    
+
+})  
+
+
+app.post('/adat_torles_tricepsz', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+
+  connection.query("DELETE FROM izomcsoport_kepek WHERE kepek_id=" +req.body.bevitel1, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log("Sikeres törlés!")
+
+    res.send("Sikeres törlés!")
+  })
+  
+  connection.end()    
+
+})  
+
+
+
+
+
+app.post('/adat_torles_vall', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+
+  connection.query("DELETE FROM izomcsoport_kepek WHERE kepek_id=" +req.body.bevitel1, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log("Sikeres törlés!")
+
+    res.send("Sikeres törlés!")
+  })
+  
+  connection.end()    
+
+})  
+
+
+
+
+
+app.post('/adat_torles_hat', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+
+  connection.query("DELETE FROM izomcsoport_kepek WHERE kepek_id=" +req.body.bevitel1, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log("Sikeres törlés!")
+
+    res.send("Sikeres törlés!")
+  })
+  
+  connection.end()    
+
+})  
+
+
+
+
+app.post('/adat_torles_hastorzs', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+
+  connection.query("DELETE FROM izomcsoport_kepek WHERE kepek_id=" +req.body.bevitel1, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log("Sikeres törlés!")
+
+    res.send("Sikeres törlés!")
+  })
+  
+  connection.end()    
+
+})  
+
+
+
+
+
+app.post('/adat_torles_vadli', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+
+  connection.query("DELETE FROM izomcsoport_kepek WHERE kepek_id=" +req.body.bevitel1, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log("Sikeres törlés!")
+
+    res.send("Sikeres törlés!")
+  })
+  
+  connection.end()    
+
+})  
+
+
+
+
+
+
+
+app.post('/adat_torles_combfar', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+
+  connection.query("DELETE FROM izomcsoport_kepek WHERE kepek_id=" +req.body.bevitel1, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log("Sikeres törlés!")
+
+    res.send("Sikeres törlés!")
+  })
+  
+  connection.end()    
+
+})  
+
+
+
+
+
+
+app.post('/adat_torles_alkar', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'zarodolgozat'
+  })
+  
+  connection.connect()
+
+  connection.query("DELETE FROM izomcsoport_kepek WHERE kepek_id=" +req.body.bevitel1, function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log("Sikeres törlés!")
+
+    res.send("Sikeres törlés!")
+  })
+  
+  connection.end()    
+
+})  
 };
 
 
